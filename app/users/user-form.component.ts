c0579bdea70b28b37634c40582931b0e1bcc088b
bjrimell <barry@Barrys-MacBook-Air.local>
2016-12-01 15:26:06 +0000

    Added Loading text

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -14,11 +14,11 @@ import { User } from '../shared/models/user';
   <form #form="ngForm" (ngSubmit)="onSubmit()" *ngIf="active">
 
     <div class="form-group" [ngClass]="{ 'has-error': name.invalid && name.touched}">
-      <input type="text" class="form-control" name="name" required [(ngModel)]="newUser.name" #name="ngModel">
+      <input type="text" class="form-control" name="name" required [(ngModel)]="newUser.name" #name="ngModel" placeholder="Name">
       <span class="help-block" *ngIf="name.invalid && name.touched">Name is required.</span>
     </div>
     <div class="form-group"  [ngClass]="{ 'has-error': username.invalid && username.touched}">
-      <input type="text" class="form-control" name="username" required [(ngModel)]="newUser.username" #username="ngModel">
+      <input type="text" class="form-control" name="username" required [(ngModel)]="newUser.username" #username="ngModel" placeholder="Username">
     <span class="help-block" *ngIf="username.invalid && username.touched">Userame is required.</span>
     </div>
     <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
